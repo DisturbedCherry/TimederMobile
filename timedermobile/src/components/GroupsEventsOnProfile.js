acf7bd608b376021243786c89ca8f8eee55279e7
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-06-17 01:36:49 +0200

    Add dynamic events and groups to user profile

--- timedermobile/src/components/GroupsEventsOnProfile.js
+++ timedermobile/src/components/GroupsEventsOnProfile.js
@@ -1,8 +1,11 @@
 // GropusEventsOnProfile.js
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './Components.css'; // You can create and style this CSS file if needed
 import SmallButton from './SmallButton';
 import { useNavigate } from 'react-router-dom';
+import {showErrorMessage} from "../services/swalService";
+import {getUserGroups} from "../services/groupService";
+import {getUserEvents} from "../services/eventService";
 
 export default function GEOP() {
   const handleButtonClick = () => {
@@ -12,17 +15,45 @@ export default function GEOP() {
 
   const navigate = useNavigate();
 
+  const [userGroups, setUserGroups] = useState([]);
+const [userEvents, setUserEvents] = useState([]);
+
+    useEffect(() => {
+        const fetchUserGroups = async () => {
+            try {
+                const response = await getUserGroups(); // Use the getUser function
+                setUserGroups(response.data);
+            } catch (error) {
+                showErrorMessage('Failed to fetch user groups.').then(r => r.dismiss);
+            }
+        };
+
+        const fetchUserEvents = async () =>  {
+            try {
+                const response = await getUserEvents(); // Use the getUser function
+                setUserEvents(response.data);
+            } catch (error) {
+                showErrorMessage('Failed to fetch user events.').then(r => r.dismiss);
+            }
+        }
+
+        fetchUserEvents();
+        fetchUserGroups();
+    }, []);
+
   const handleJuwenaliaButtonClick = () => {
     navigate('/EventScreen')
   }
 
   return (
     <div className='geop-div'>
-      <SmallButton text='Turniej AZS' onClick={handleButtonClick}/>
-      <SmallButton text='Juwenalia' onClick={handleJuwenaliaButtonClick}/>
-      <SmallButton text='Maraton' onClick={handleButtonClick}/>
-      <SmallButton text='Piątkowe wyjście' onClick={handleButtonClick}/>
-      <SmallButton text='APR' onClick={handleButtonClick}/>
+        <SmallButton text='Juwenalia' onClick={handleJuwenaliaButtonClick}/>
+        {userGroups.map((group, index) => (
+            <SmallButton key={index} text={group.name} onClick={handleButtonClick}/>
+        ))}
+        {userEvents.map((event, index) => (
+            <SmallButton key={index} text={event.name} onClick={handleButtonClick}/>
+        ))}
     </div>
   );
 }
