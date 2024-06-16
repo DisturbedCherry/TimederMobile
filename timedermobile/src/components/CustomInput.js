fbd9f16481c07afa5beee3f91d50874a3ed9afdc
DisturbedCherry <jakub.t.wislicki@gmail.com>
2024-06-16 12:50:44 +0200

    Minor changes

---
+++ timedermobile/src/components/CustomInput.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import './Components.css';
+
+const InputBox = ({placeholdertext}) => {
+  const [inputValue, setInputValue] = useState('');
+
+  const handleChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  return (
+    <input
+      type="text"
+      value={inputValue}
+      onChange={handleChange}
+      className={`input-box ${inputValue.length >= 3 ? 'valid' : ''}`}
+      placeholder={placeholdertext}
+    />
+  );
+};
+
+export default InputBox;
