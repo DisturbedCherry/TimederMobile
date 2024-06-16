c9f95656eb661ef9c9e4a2379eea6dc526a115b6
DisturbedCherry <jakub.t.wislicki@gmail.com>
2024-06-16 12:50:34 +0200

    Finished CreateEvenScreen

--- timedermobile/src/pages/CreateEvenScreen/CreateEvenScreen.js
+++ timedermobile/src/pages/CreateEvenScreen/CreateEvenScreen.js
@@ -1,7 +1,29 @@
-export default function CreateEvenScreen() {
+import React from 'react';
+import './CreateEvenScreen.css';
+import Sidebar from '../../components/Sidebar';
+import BackButton from '../../components/CornerButton';
+import PinButton from '../../components/PinButton';
+import { IoIosArrowBack } from "react-icons/io";
+import { TiPin } from "react-icons/ti";
+import Input from '../../components/CustomInput'
+
+export default function NotificationScreen() {
+  const handleButtonClick = () => {
+    alert('Button clicked!');
+  };
+
   return (
-    <>
-      <h1>Create Even Screen</h1>
-    </>
-  )
+    <div className="create-event-screen">
+      <BackButton className="back-button" icon={<IoIosArrowBack />} onClick={handleButtonClick}/>
+      <PinButton className="pin-button" icon={<TiPin  />} onClick={handleButtonClick}/>
+      <Sidebar/>
+      <div className='button-container-notification'>
+        <h1>Notifications</h1>
+        <Input placeholdertext='test'/>
+        <Input />
+        <Input />
+        <Input />
+      </div>
+    </div>
+  );
 }
