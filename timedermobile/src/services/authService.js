c371c3fa20d6daacf0cc084dce619decfd4e62cb
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-06-16 22:59:17 +0200

    Add swal and auth services

---
+++ timedermobile/src/services/authService.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+import { jwtDecode } from 'jwt-decode';
+
+const AuthService = {
+    getToken: function() {
+        return localStorage.getItem('token');
+    },
+
+    decodeToken: function() {
+        const token = this.getToken();
+        return jwtDecode(token);
+    },
+
+    getUserId: function() {
+        const decoded = this.decodeToken();
+        return decoded.userId;
+    },
+
+    setAxiosAuthHeader: function() {
+        const token = this.getToken();
+        if (token) {
+            axios.defaults.headers.common['Authorization'] = 'Bearer ' + token;
+        } else {
+            delete axios.defaults.headers.common['Authorization'];
+        }
+    },
+
+    login: function(index, password)  {
+        AuthService.setAxiosAuthHeader();
+        console.log("index: " + index + " password: " + password);
+        return  axios.post(`/auth/authenticate`, {index: index, password: password});
+    }
+};
+
+export default AuthService;
