9a311ea6cf925778b564adc65c1c17c3267ea51c
DisturbedCherry <jakub.t.wislicki@gmail.com>
2024-06-16 12:50:10 +0200

    Finished Notification Screen

--- timedermobile/src/pages/NotificationScreen/NotificationScreen.js
+++ timedermobile/src/pages/NotificationScreen/NotificationScreen.js
@@ -1,7 +1,31 @@
+import React from 'react';
+import './NotificationScreen.css';
+import Sidebar from '../../components/Sidebar';
+import BackButton from '../../components/CornerButton';
+import PinButton from '../../components/PinButton';
+import { IoIosArrowBack } from "react-icons/io";
+import { TiPin } from "react-icons/ti";
+import SmallButton from '../../components/SmallButton'
+
 export default function NotificationScreen() {
+  const handleButtonClick = () => {
+    alert('Button clicked!');
+  };
+
   return (
-    <>
-      <h1>Notification Screen</h1>
-    </>
-  )
+    <div className="notification-screen">
+      <BackButton className="back-button" icon={<IoIosArrowBack />} onClick={handleButtonClick}/>
+      <PinButton className="pin-button" icon={<TiPin  />} onClick={handleButtonClick}/>
+      <Sidebar/>
+      <div className='button-container-notification'>
+        <h1>Notifications</h1>
+        <SmallButton text='You have been added to "Wejkusie" group' onClick={handleButtonClick}/>
+        <SmallButton text='You have been removed from "PIO project" group' onClick={handleButtonClick}/>
+        <SmallButton text='You have joined "Flanki 25 marca" event' onClick={handleButtonClick}/>
+        <SmallButton text='You have created "Degustacja monsterków" event' onClick={handleButtonClick}/>
+        <SmallButton text='You have left "Party Piotrkowska" event' onClick={handleButtonClick}/>
+        <SmallButton text='You have left "Party Piotrkowska" event' onClick={handleButtonClick}/>
+      </div>
+    </div>
+  );
 }
