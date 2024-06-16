13b9140aa2ba3154fc2f845c66224d55d0ab0fd6
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-06-17 00:34:23 +0200

    Add functionality to fetch notifications from server

--- timedermobile/src/pages/NotificationScreen/NotificationScreen.js
+++ timedermobile/src/pages/NotificationScreen/NotificationScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './NotificationScreen.css';
 import Sidebar from '../../components/Sidebar';
 import BackButton from '../../components/CornerButton';
@@ -7,32 +7,45 @@ import { IoIosArrowBack } from "react-icons/io";
 import { TiPin } from "react-icons/ti";
 import SmallButton from '../../components/SmallButton'
 import { useNavigate } from 'react-router-dom';
+import { getNotifications } from '../../services/notificationService';
+import {showErrorMessage} from "../../services/swalService";
 
 export default function NotificationScreen() {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
+    const [notifications, setNotifications] = useState([]);
 
-  const handleBackButtonClick = () => {
-    navigate(-1);
-  };
+    useEffect(() => {
+        const fetchNotifications = async () => {
+            try {
+                const response = await getNotifications();
+                setNotifications(response.data);
+            } catch (error) {
+                showErrorMessage('Failed to fetch notifications.').then(r => r.dismiss);
+            }
+        };
 
-  const handleButtonClick = () => {
-    alert('Button clicked!');
-  };
-  
-  return (
-    <div className="notification-screen">
-      <BackButton className="back-button" icon={<IoIosArrowBack />} onClick={handleBackButtonClick}/>
-      <PinButton className="pin-button" icon={<TiPin  />} onClick={handleButtonClick}/>
-      <Sidebar/>
-      <div className='button-container-notification'>
-        <h1>Notifications</h1>
-        <SmallButton text='You have been added to "Wejkusie" group' onClick={handleButtonClick}/>
-        <SmallButton text='You have been removed from "PIO project" group' onClick={handleButtonClick}/>
-        <SmallButton text='You have joined "Flanki 25 marca" event' onClick={handleButtonClick}/>
-        <SmallButton text='You have created "Degustacja monsterków" event' onClick={handleButtonClick}/>
-        <SmallButton text='You have left "Party Piotrkowska" event' onClick={handleButtonClick}/>
-        <SmallButton text='You have left "Party Piotrkowska" event' onClick={handleButtonClick}/>
-      </div>
-    </div>
-  );
+        fetchNotifications();
+        }, []);
+
+    const handleBackButtonClick = () => {
+        navigate(-1);
+    };
+
+    const handleButtonClick = () => {
+        alert('Button clicked!');
+    };
+
+    return (
+        <div className="notification-screen">
+            <BackButton className="back-button" icon={<IoIosArrowBack />} onClick={handleBackButtonClick}/>
+            <PinButton className="pin-button" icon={<TiPin  />} onClick={handleButtonClick}/>
+            <Sidebar/>
+            <div className='button-container-notification'>
+                <h1>Notifications</h1>
+                {notifications.map((notification, index) => (
+                    <SmallButton key={index} text={notification.content} onClick={handleButtonClick}/>
+                ))}
+            </div>
+        </div>
+    );
 }
