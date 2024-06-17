c441d7d693d467619fac022e19873b8c46a86103
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-06-17 02:27:12 +0200

    Add service method to call createGroup endpoint

--- timedermobile/src/services/groupService.js
+++ timedermobile/src/services/groupService.js
@@ -6,3 +6,16 @@ export const getUserGroups = () => {
     AuthService.setAxiosAuthHeader();
     return axios.get('/groups/users/' + authService.getUserId());
 }
+
+export const createGroup = (inputValues, userId) => {
+    AuthService.setAxiosAuthHeader();
+    return axios.post('/groups/', {
+        name: inputValues.name,
+        description: inputValues.description,
+        currentSize: 1,
+        totalSize: inputValues.totalSize,
+        isPrivate: !inputValues.isPrivate,
+        joinCode: inputValues.joinCode,
+        ownerId: userId,
+    });
+}
