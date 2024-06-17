08d7f2f9c876ca258e640f883d917687dc2048e1
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-06-17 02:27:24 +0200

    Improve input components

--- timedermobile/src/components/CustomInput.js
+++ timedermobile/src/components/CustomInput.js
@@ -1,22 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './Components.css';
 
-const InputBox = ({placeholdertext}) => {
-  const [inputValue, setInputValue] = useState('');
-
-  const handleChange = (event) => {
-    setInputValue(event.target.value);
-  };
-
-  return (
-    <input
-      type="text"
-      value={inputValue}
-      onChange={handleChange}
-      className={`input-box-vital ${inputValue.length >= 3 ? 'valid' : ''}`}
-      placeholder={placeholdertext}
-    />
-  );
+const InputBox = ({ placeholdertext, onChange, value }) => {
+    return (
+        <input
+            type="text"
+            onChange={onChange}
+            value={value}
+            className={`input-box-vital ${value.length >= 3 ? 'valid' : ''}`}
+            placeholder={placeholdertext}
+        />
+    );
 };
 
 export default InputBox;
